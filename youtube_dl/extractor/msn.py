abb854f047fafa06c21e8081ac28c20b91537125
TRox1972 <archcr8@gmail.com>
2016-05-18 01:30:46 +0200

    [MSN] cleanup

--- youtube_dl/extractor/msn.py
+++ youtube_dl/extractor/msn.py
@@ -16,7 +16,6 @@ class MSNIE(InfoExtractor):
 	def _real_extract(self, url):
 		video_id = self._match_id(url)
 
-		#self.report_download_webpage(video_id)
 		webpage = self._download_webpage(url, video_id)
 
 		self.report_extraction(video_id)
@@ -25,45 +24,12 @@ class MSNIE(InfoExtractor):
 		raw_data = raw_data.replace('data-metadata="', '')[:-1]
 		data = self._parse_json(raw_data, video_id)
 
-		#_available_formats = []
-		#[for formatCode in data.get('videoFiles') _available_formats.append(int(formatCode)]
-
-		_available_formats = ['101', '102', '103', '104']
-
-		_video_extensions = {
-			'101': 'mp4',
-			'102': 'mp4',
-			'103': 'mp4',
-			'104': 'mp4'
-		}
-
-		_video_dimensions = {
-			'101': (320, 180),
-			'102': (640, 360),
-			'103': (640, 360),
-			'104': (640, 360),
-		} # difference between 102, 103 and 104?
-
-
-		# TODO: Fix subtitles
-		# ( http://prod.video.msn.com/tenant/amp/entityid/BBqQYNE?blobrefkey=en-us&$blob=1 )
-		# ( http://prod.video.msn.com/tenant/amp/entityid/%s?blobrefkey=%s&$blob=1 % ( video_id, lang ) )
-
-
 		formats = []
-		#[for videoFile in data.get('videoFiles') formats.append(videoFile('url').replace('&amp;', '&'))]
-
 		for video_file in data.get('videoFiles'):
 			#if 'blobrefkey' in video_file:
-			if 1<2:
-				formats.append(video_file['url'].replace('&amp;', '&'))
-
-		#for f in formats:
-		#	print(f)
-		#quit()
-
-		#print(formats[1])
-
+			if 1<2: # remove format 1001 (?)
+				formats.append(video_file['url'].replace('&amp;', '&')]
+		formats =  formats[1:] # need more elegant solution
 
 		return {
 			'id': video_id,
@@ -73,5 +39,4 @@ class MSNIE(InfoExtractor):
 			'description': data.get('description'),
 			'ext': 'mp4',
 			#'formats': formats[1:4],
-
 		}
